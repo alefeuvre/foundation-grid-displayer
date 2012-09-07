91392c61f2a93b33f9f02f9e0c7a9af2ecf41ee3
Antoine Lefeuvre <lefeuvre@novius.com>
2012-09-07 23:39:01 +0200

    New Foundation 3.0 bookmarklet

--- bookmarklet-3.js
+++ bookmarklet-3.js
@@ -10,14 +10,21 @@ if (typeof jQuery === "undefined") {
   
   // Setters
   var setGridColor = function(gridColor) {  
-    $("#grid-displayer .one").css("background-color", gridColor);
-  }
-  var setGridOpacity = function(gridOpacity) {  
+    $("#grid-displayer .one:even").css("background-color", gridColor); // 0-based indexing means that, counter-intuitively, :odd selects the 2th element, 4th element, ...
+    setBorderStyle();
+  },
+  setGridOpacity = function(gridOpacity) {  
     $("#grid-displayer .one").css("opacity", gridOpacity);
-  }
-  var setGridZindex = function(gridZindex) {  
+    setBorderStyle();
+  },
+  setGridZindex = function(gridZindex) {  
     $("#grid-displayer").css("z-index", gridZindex);
-  }
+  },
+  setBorderStyle = function() { // I wish border-opacity existed  
+    var currentOpacity = $("#grid-displayer .one").css("opacity"),
+        rgbaColor = $("#grid-displayer .one:even").css("background-color").replace('rgb', 'rgba').replace(')',', ' + currentOpacity + ')');
+    $("#grid-displayer .row").css("border-right", "2px solid " + rgbaColor);
+  };
   
   // Removes grid displayer when the bookmarklet is clicked for a second time
   if ($("#grid-displayer").length) {
@@ -30,14 +37,10 @@ if (typeof jQuery === "undefined") {
       dataGridOpacity = $("body").data("grid-opacity"),
       gdtOpacity      = (typeof dataGridOpacity === "undefined") ? "0.3" : dataGridOpacity,
       dataGridZindex  = $("body").data("grid-zindex"),
-      gdtZindex       = (typeof dataGridZindex === "undefined") ? "0" : dataGridZindex,
-      containerIsUsed = $(".container").length,
-      gdtContainer    = (containerIsUsed) ? "checked" : "";
+      gdtZindex       = (typeof dataGridZindex === "undefined") ? "0" : dataGridZindex;
     
     // Grid and toolbar HTML
-    gridHtml  = "<div id=\"grid-displayer\"";
-    gridHtml += (containerIsUsed) ? " class=\"container\"" : "";
-    gridHtml += "><div class=\"row\">";
+    gridHtml  = "<div id=\"grid-displayer\"><div class=\"row\">";
     for(var i = 0; i < 12; i++) {
       gridHtml += "<div class=\"one columns\">&nbsp;</div>";
     }
@@ -48,13 +51,12 @@ if (typeof jQuery === "undefined") {
     gridToolsHtml += "  <div><label for=\"gdt-color\">Columns colour</label> <input type=\"text\" id=\"gdt-color\" value=\"" + gdtColor + "\" /></div>";
     gridToolsHtml += "  <div><label for=\"gdt-opacity\">Opacity</label> <input type=\"text\" id=\"gdt-opacity\" value=\"" + gdtOpacity + "\" /></div>";
     gridToolsHtml += "  <div><label for=\"gdt-zindex\">z-index</label> <input type=\"text\" id=\"gdt-zindex\" value=\"" + gdtZindex + "\" /></div>";
-    gridToolsHtml += "  <div><label for=\"gdt-container\">With container</label> <input type=\"checkbox\" id=\"gdt-container\" " + gdtContainer + " /></div>";
     gridToolsHtml += "  <div>update on blur</div>";
     gridToolsHtml += "  <div><a href=\"#\" id=\"gdt-close\">Close</a></div>";
     gridToolsHtml += "</div>";
     
-    // Init
-    $("head").append("<link rel='stylesheet' type='text/css' href='http://alefeuvre.github.com/foundation-grid-displayer/stylesheets/bookmarklet-style.css'>");
+    // Init - http://alefeuvre.github.com/foundation-grid-displayer/stylesheets/
+    $("head").append("<link rel='stylesheet' type='text/css' href='http://s311709191.onlinehome.fr/perso/bookmarklet-style-3.css'>");
     $("body").prepend(gridHtml).prepend(gridToolsHtml);  
     $("#grid-displayer-tools").delay(1200).fadeTo("slow",0.1);  
         
@@ -67,7 +69,7 @@ if (typeof jQuery === "undefined") {
     }
     if (typeof dataGridZindex !== "undefined") {
       setGridZindex(gdtZindex);
-    }
+    }   
     
     // Updates
     $("#grid-displayer-tools #gdt-color").change(function() {
@@ -79,9 +81,6 @@ if (typeof jQuery === "undefined") {
     $("#grid-displayer-tools #gdt-zindex").change(function() {
       setGridZindex($(this).val());
     });    
-    $("#grid-displayer-tools #gdt-container").change(function() {    
-      $("#grid-displayer").toggleClass("container");
-    });
     
     $("#grid-displayer-tools #gdt-close").click(function() {
       removeGridDisplayer();
