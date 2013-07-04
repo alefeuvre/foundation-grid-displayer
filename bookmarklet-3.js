8f2c8fe7321ef3f98c05a8a385548adf66f5b084
Antoine Lefeuvre <lefeuvre@novius.com>
2013-07-04 16:04:40 +0200

    Old versions for legacy

---
+++ bookmarklet-3.js
@@ -0,0 +1,5 @@
+// This version is outdated, see gd-bookmarklet.js
+alert("Sorry, this version of the grid displayer bookmarklet is no longer available.\n"
+    + "Don't worry though, a newer, better bookmarklet is available.\n"
+    + "Just hit OK to be redirected to the install page (it opens in a new tab/window).");
+window.open("http://alefeuvre.github.io/foundation-grid-displayer/");    
